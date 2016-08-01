f528ea4c75105ae25cb6d428dc2a8f6aa3e18b2b
manamkasibodam <sororoshervin@gmail.com>
2016-08-01 17:25:29 +0430

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -1261,3 +1261,47 @@ function banall_by_reply(extra, success, result)
 		return
   end
 end
+function get_receiver_api(msg)
+  if msg.to.type == 'user' then
+    return msg.from.id
+  end
+  if msg.to.type == 'chat' then
+    return '-'..msg.to.id
+  end
+  if msg.to.type == 'channel' then
+    return '-100'..msg.to.id
+  end
+end
+function send_api_msg(msg, receiver, text, disable_web_page_preview, markdown,inline_text,inline_url)
+  
+  
+  local api_key = '257954119:AAEZd8kDlWuwPqYPR674h8o-GC7JuhMXVjY'
+  
+  
+  local url_api = 'https://api.telegram.org/bot'..api_key
+      ..'/sendMessage?chat_id='..receiver..'&text='..URL.escape(text)
+  if disable_web_page_preview == true then
+    url_api = url_api..'&disable_web_page_preview=true'
+  end
+  if markdown == 'md' then
+    url_api = url_api..'&parse_mode=Markdown'
+  elseif markdown == 'html' then
+    url_api = url_api..'&parse_mode=HTML'
+  end
+if inline_text and inline_url then
+url_api = url_api..'&reply_markup={"inline_keyboard":[[{"text":"'..inline_text..'","url":"'..inline_url..'"}]]}'
+end
+  local dat, res = https.request(url_api)
+  local test = print(url_api)
+  if res == 400 then
+    reply_msg(msg.id, 'بو عنت همه جارو برداشته (تو کیبورد ریدی)...', ok_cb, true)
+  end
+end
+function send_api_keyboard(msg, receiver, text, keyboard)
+  local api_key = '257954119:AAEZd8kDlWuwPqYPR674h8o-GC7JuhMXVjY'
+  local url_api = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='.. receiver..'&parse_mode=markdown&&text='..URL.escape(text)..'&disable_web_page_preview=true&reply_markup='..json:encode(keyboard)
+  local dat, res = https.request(url_api)
+  if res == 400 then
+    reply_msg(msg.id, 'بو عنت همه جارو برداشته (تو کیبورد ریدی)...', ok_cb, true)
+  end
+  end
